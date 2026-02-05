a676a06e1c0cdc938ed4746846e5795be31ef671
Parkgwiyoung <pgwi11@naver.com>
2026-02-05 11:48:37 +0900

    rq, t1 - r

---
+++ src/test/java/RqTest.java
@@ -0,0 +1,16 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class RqTest {
+
+    @Test
+    @DisplayName("rq.getActionName()")
+    void t1() {
+
+        Rq rq = new Rq("삭제?id=1");
+        String actionName = rq.getAction();
+        Assertions.assertThat(actionName).isEqualTo("삭제");
+
+    }
+}
