3dc1a395725fc7a1132fada9bfbc1ca4ddfc36e8
Parkgwiyoung <pgwi11@naver.com>
2026-02-05 12:47:14 +0900

    t4 - b1,b2

--- src/main/java/Rq.java
+++ src/main/java/Rq.java
@@ -1,28 +1,31 @@
 public class Rq {
 
-
     private String cmd;
 
     public Rq(String cmd) {
         this.cmd = cmd;
     }
 
-      public String getAction() {
+    public String getActionName() {
+
 //        if (cmd.equals("삭제?id=1")) {
 //            return "삭제";
 //        }
 //        if (cmd.equals("수정?id=1")) {
 //            return "수정";
 //        }
+
         return cmd.split("\\?")[0];
     }
-    public String getParam(String key){
-        if(cmd.equals("목록?keyword=자바")){
-            return "자바";
-        }
 
-        if(cmd.equals("목록?keywordType=title")){
-            return "title";
+    public String getParam(String key) {
+
+        String param = cmd.split("\\?")[1];
+        String paramKey = param.split("=")[0];
+        String paramValue = param.split("=")[1];
+
+        if(paramKey.equals(key)) {
+            return paramValue;
         }
 
         return "";
